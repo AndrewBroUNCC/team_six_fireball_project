c026d61c71c20b58ade83f6ec44206c97d123d70
JustinLeiner <jleiner1@uncc.edu>
2023-04-13 16:25:27 -0400

    General Info updated!!!!

--- app/src/main/java/com/example/team_six_fireball_project/GeneralInfoFragment.java
+++ app/src/main/java/com/example/team_six_fireball_project/GeneralInfoFragment.java
@@ -1,5 +1,7 @@
 package com.example.team_six_fireball_project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class GeneralInfoFragment extends Fragment {
@@ -31,6 +37,8 @@ public class GeneralInfoFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,6 +46,67 @@ public class GeneralInfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_general_info, container, false);
         getActivity().setTitle("Information");
 
+
+        /* Example block for wanting an onclick event (Ex. click on text, button, image,e tc..) */
+
+        TextView testTextView = view.findViewById(R.id.textViewTest);
+        testTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri webpage = Uri.parse("https://www.theguardian.com/science/2013/nov/06/chelyabinsk-meteor-russia");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+
+            }
+        });
+
+
+        Button button_Recent_Fireball_Activity = view.findViewById(R.id.button_Recent_Fireball_Activity);
+
+        button_Recent_Fireball_Activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri webpage = Uri.parse("https://fireball.imo.net/members/imo_view/browse_events");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+
+            }
+        });
+
+        Button button_report = view.findViewById(R.id.button_report);
+
+        button_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri webpage = Uri.parse("https://ams.imo.net/members/imo/report_intro/");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+
+            }
+        });
+
+
+
+        Button button_faq = view.findViewById(R.id.button_faq);
+
+        button_faq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri webpage = Uri.parse("https://www.amsmeteors.org/fireballs/faqf/");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+
+            }
+        });
+
+
+
         return view;
     }
+
+
 }
